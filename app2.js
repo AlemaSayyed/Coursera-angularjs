a1ee5cf0be9b3ee6322b120a4c26b70779ceebaa
Alema Sayyed <alema@gmail.com>
2020-05-15 02:26:37 +0530

    'module1-solution'

---
+++ app2.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular.module('DIApp',[])
+  .controller('DIController', DIController);
+
+  function DIController($scope,$filter) {
+    $scope.name = "Alema";
+
+    $scope.upper = function () {
+      var uppCase = $filter('uppercase');
+      $scope.name= uppCase($scope.name);
+
+    }
+
+  }
+})();
